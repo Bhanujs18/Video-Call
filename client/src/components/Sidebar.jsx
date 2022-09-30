31ec9b6a97cc66920747dde3e60cce0511dc7cf8
Bhanu <94670318+Bhanujs18@users.noreply.github.com>
2022-09-30 15:49:11 +0530

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,45 +1,90 @@
 import React, { useState, useContext } from 'react';
-import { Button, TextField} from '@material-ui/core';
+import { Button, TextField, Grid, Typography, Container, Paper, AppBar } from '@material-ui/core';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import {  Assignment, Phone, PhoneDisabled } from '@material-ui/icons';
+import { Assignment, Phone, PhoneDisabled } from '@material-ui/icons';
+import { makeStyles } from '@material-ui/core/styles';
+
 import { SocketContext } from '../Context';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap:'wrap',
+    
+  },
+  gridContainer: {
+    flexWrap:'wrap',
+    width: '100%',
+    height:'100%',
+     display: 'flex',
+    [theme.breakpoints.down('xs')]: {
+      
+    },
+  },
+  container: {
+    flexWrap:'wrap',
+    margin: 9,
+    padding: 0,
+
+    [theme.breakpoints.down('xs')]: {
+      width: '100%',
+      
+    },
+  },
+  margin: {
+    flexWrap:'wrap',
+    margin: 9,
+  },
+  padding: {
+    display: 'flex',
+   
+    padding: 10,
+  },
+  paper: {
+    flexWrap:'wrap',
+    padding: '10px 20px',
+    display: 'flex',
+    boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+  },
+}));
 
 const Sidebar = ({ children }) => {
   const { me, callAccepted, name, setName, callEnded, leaveCall, callUser } = useContext(SocketContext);
   const [idToCall, setIdToCall] = useState('');
+  const classes = useStyles();
 
   return (
-        <div id='header'>
-          <div>
-            <h1 className='text'>  • Live </h1>
-          </div>
-        <form noValidate autoComplete="off" className='inputdiv'>
-          <div className='input'>
-              <TextField label="Name" color='inherit' value={name} onChange={(e) => setName(e.target.value)} />
-              <CopyToClipboard text={me} >
-                <Button variant="contained" color="inherit" startIcon={<Assignment fontSize="large" />}>
-                   Code
+    <Container className={classes.container}>
+      <Paper elevation={10} className={classes.paper}>
+        <form className={classes.root} noValidate autoComplete="off">
+          <Grid container className={classes.gridContainer}>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography gutterBottom variant="h6"></Typography>
+              <TextField label="Name" value={name} onChange={(e) => setName(e.target.value)} fullWidth />
+              <CopyToClipboard text={me} className={classes.margin}>
+                <Button variant="contained" color="primary" fullWidth startIcon={<Assignment fontSize="large" />}>
+                  Code
                 </Button>
               </CopyToClipboard>
-              </div>
-              <div className='input'>
-              <TextField label="Enter Code"  value={idToCall} onChange={(e) => setIdToCall(e.target.value)}  />
+            </Grid>
+            <Grid item xs={12} md={6} className={classes.padding}>
+              <Typography gutterBottom variant="h6"></Typography>
+              <TextField label="ID to call" value={idToCall} onChange={(e) => setIdToCall(e.target.value)} fullWidth />
               {callAccepted && !callEnded ? (
-                <Button variant="contained"  color="secondary" startIcon={<PhoneDisabled fontSize="large" />}  onClick={leaveCall} >
+                <Button variant="contained"  startIcon={<PhoneDisabled fontSize="large" />} fullWidth onClick={leaveCall} className={classes.margin}>
                   Hang Up
                 </Button>
               ) : (
-                <Button variant="contained"  id='colorcall' startIcon={<Phone fontSize="large" />}  onClick={() => callUser(idToCall)} >
+                <Button variant="contained" id='callbutton' startIcon={<Phone fontSize="large" />} fullWidth onClick={() => callUser(idToCall)} className={classes.margin}>
                   Call
                 </Button>
-                
               )}
-              </div>
+            </Grid>
+          </Grid>
         </form>
         {children}
-        </div>
-   
+      </Paper>
+    </Container>
    
   );
 };
