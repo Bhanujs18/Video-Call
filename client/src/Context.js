3be138dc97ff3eceff9b1295c38b3b772004c811
Bhanujs18 <bhanupratap19995@gmail.com>
2023-09-20 13:03:36 +0530

    updated

--- client/src/Context.js
+++ client/src/Context.js
@@ -4,8 +4,8 @@ import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000/');
-// const socket = io('https://video-chat-byjs.herokuapp.com/');
+// const socket = io('http://localhost:5000/');
+const socket = io('https://web-production-5a4a.up.railway.app/');
 
 const ContextProvider = ({ children }) => {
   const [callAccepted, setCallAccepted] = useState(false);
